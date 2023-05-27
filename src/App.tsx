4ddb340dd7c111450b276d76f70f7a1288d91efc
Abytova Saikal <saikalabytova1@gmail.com>
2023-05-27 12:33:36 +0600

    Created base function createMainTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const createMainTable = () => {
+    type Game = {
+      hasItem: boolean,
+      clicked: boolean,
+    };
+
+    let arr: Game[] =[];
+
+    for(let i = 0; i < 36; i++) {
+      arr.push({
+        hasItem: false,
+        clicked: false,
+      });
+    }
+
+    const randomElOfArr = Math.floor(Math.random() * arr.length);
+    arr[randomElOfArr] = {hasItem: true, clicked: false};
+    console.log(arr[randomElOfArr]);
+  };
+
+  createMainTable();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
